3b880022977ab1a44c3a3b9d2656f74ea1b7873f
Dan Brough <dan@danbrough.org>
2024-03-13 20:57:47 +1300

    update

--- conan/tests/simplecmake/run.sh
+++ conan/tests/simplecmake/run.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 
-rm -rf build
+cd `dirname $0`
+rm -rf build CMakeUserPresets.json 2> /dev/null
+
 
 conan install . --output-folder=build --build=missing
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
+
+printf "\n### running compressor...\n\n"
 ./compressor
