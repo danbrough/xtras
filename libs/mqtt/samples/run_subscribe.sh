4f170421a550fee77c7aa191a28b2cd34fa2b68b
Dan Brough <dan@danbrough.org>
2024-03-28 16:21:58 +1300

    update

--- libs/mqtt/samples/run_subscribe.sh
+++ libs/mqtt/samples/run_subscribe.sh
@@ -9,12 +9,14 @@ MQTT=/home/dan/workspace/xtras/xtras/libs/mqtt/linuxX64/1.3.13
 
 [ -f $EXE ] && rm $EXE
 
-OPTS="-I. -I$MQTT/include -L$MQTT/lib -lpaho-mqtt3as"
+#gcc program.o -llib1 -Wl,-Bstatic -llib2 -Wl,-Bdynamic -llib3
 
+OPTS="-I. -I$MQTT/include -L$MQTT/lib -Wl,-Bstatic -llib2 -lpaho-mqtt3as"
+export LD_LIBRARY_PATH=$MQTT/lib
 SRC=MQTTAsync_subscribe.c
 
 echo gcc $OPTS -o $EXE $SRC
 gcc $OPTS -o $EXE $SRC || exit 1
 
-LD_LIBRARY_PATH=$MQTT/lib ./$EXE
+./$EXE
 
