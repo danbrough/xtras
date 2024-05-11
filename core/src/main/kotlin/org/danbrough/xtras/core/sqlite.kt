f5511f830a5136a9ce8a27cc825575b5f10d705d
Dan Brough <dan@danbrough.org>
2024-05-11 15:25:21 +1200

    update

--- core/src/main/kotlin/org/danbrough/xtras/core/sqlite.kt
+++ core/src/main/kotlin/org/danbrough/xtras/core/sqlite.kt
@@ -6,6 +6,7 @@ import org.danbrough.xtras.environmentNDK
 import org.danbrough.xtras.hostTriplet
 import org.danbrough.xtras.kotlinTargetName
 import org.danbrough.xtras.mixedPath
+import org.danbrough.xtras.pathOf
 import org.danbrough.xtras.projectProperty
 import org.danbrough.xtras.registerXtrasGitLibrary
 import org.danbrough.xtras.tasks.SourceTaskName
@@ -32,11 +33,15 @@ fun Project.sqlite(extnName: String = "sqlite", block: XtrasLibrary.() -> Unit) 
 		environment { target ->
 			if (target.family == Family.ANDROID) {
 				environmentNDK(xtras, target)
-				put("CXX","${get("PREFIX")}clang++")
-				put("CC","${get("PREFIX")}clang")
+				put("CXX", "${get("PREFIX")}clang++")
+				put("CC", "${get("PREFIX")}clang")
 				//environmentKonan(this@registerXtrasGitLibrary, target)
 			} else if (target == KonanTarget.LINUX_ARM64 || target == KonanTarget.MACOS_ARM64 || target == KonanTarget.LINUX_X64) {// || ((target == KonanTarget.MINGW_X64) && HostManager.hostIsMingw)) {
 				environmentKonan(this@registerXtrasGitLibrary, target)
+			}
+
+			if (target.family == Family.LINUX) {
+				put("PATH", pathOf("/usr/x86_64-w64-mingw32/bin", get("PATH")))
 			}
 		}
 
