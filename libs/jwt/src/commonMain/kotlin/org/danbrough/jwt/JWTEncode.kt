228b8a43137105e1a0ab58aa1ce0805539b3185e
Dan Brough <dan@danbrough.org>
2024-05-25 16:13:51 +1200

    update

---
+++ libs/jwt/src/commonMain/kotlin/org/danbrough/jwt/JWTEncode.kt
@@ -0,0 +1,3 @@
+package org.danbrough.jwt
+
+expect class JWTEncode
--- libs/jwt/src/commonMain/kotlin/org/danbrough/jwt/jwt.kt
+++ libs/jwt/src/commonMain/kotlin/org/danbrough/jwt/jwt.kt
@@ -4,3 +4,12 @@ package org.danbrough.jwt
 internal val log = klog.logger("JWT")
 
 
+
+expect fun <R> jwtEncode(block: JWTEncode.()->R): R
+
+
+
+
+expect fun <R> jwtDecode(token:String,alg: JwtAlg,secret:UByteArray,block: JWTDecode.()->R): R
+
+
