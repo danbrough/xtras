0d92ca17427e1c21a4c8485a1496ad719e0f30f8
Dan Brough <dan@danbrough.org>
2024-07-17 07:11:02 +1200

    update

--- plugin/src/main/kotlin/org/danbrough/xtras/Xtras.kt
+++ plugin/src/main/kotlin/org/danbrough/xtras/Xtras.kt
@@ -12,6 +12,17 @@ import org.jetbrains.kotlin.konan.target.HostManager
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import java.io.File
 
+@DslMarker
+annotation class XtrasDSL
+
+const val XTRAS_GROUP = "org.danbrough.xtras"
+const val XTRAS_REPO_NAME = "Xtras"
+const val XTRAS_LOCAL_REPO_NAME = "Local"
+const val XTRAS_SONATYPE_REPO_NAME = "Sonatype"
+const val XTRAS_TASK_GROUP = "xtras"
+const val ENV_BUILD_DIR = "BUILD_DIR"
+const val XTRAS_EXTENSION_NAME = "xtrasExtension"
+
 abstract class Xtras(val project: Project) {
 
   companion object Properties {
