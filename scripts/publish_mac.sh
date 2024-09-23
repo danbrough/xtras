e997e133725ed156731f1847338c7ff70f4c1f2a
Dan Brough <dan@danbrough.org>
2024-09-23 20:15:45 +1200

    update

--- scripts/publish_mac.sh
+++ scripts/publish_mac.sh
@@ -1,4 +1,4 @@
-PACKAGE=:openssl
+PACKAGE=:$1
 ./gradlew -Psonatype.repoID=orgdanbrough-1783 -Psonatype.closeRepository=0 \
 $PACKAGE:publishMacosArm64PublicationToSonatypeRepository \
 $PACKAGE:publishMacosX64PublicationToSonatypeRepository \
