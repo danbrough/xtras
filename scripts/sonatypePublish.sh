3acd391383e5be4380d5a096fafaad59fbebbc45
Dan Brough <dan@danbrough.org>
2025-02-09 20:22:56 -0600

    update

---
+++ scripts/sonatypePublish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo 'usage: OPEN=? CLOSE=? ID=? '$0'  [gradle package]'
+
+[ ! -f gradle.properties ] && echo gradle.properties not found && exit 0
+
+[ -z "$CLOSE" ] && CLOSE=0
+[ -z "$OPEN" ] && OPEN=0
+
+if [ -z "$ID" ]; then
+	REPO=""
+else
+	REPO="-Psonatype.repoID=orgdanbrough-$ID"
+fi
+
+echo open $OPEN close $CLOSE REPO $REPO
+exit 0
+./gradle -Ppublish.docs=1 -Ppublish.sign=1 \
+	-Psonatype.closeRepository=$CLOSE -Psonatype.openRepository=$OPEN \
+	$REPO $@
+
+
+
