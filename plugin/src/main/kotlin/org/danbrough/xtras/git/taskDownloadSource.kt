79abeb8f253235b4d80732f973ea51ca9fbde719
Dan Brough <dan@danbrough.org>
2026-02-09 12:14:33 -0600

    update

--- plugin/src/main/kotlin/org/danbrough/xtras/git/taskDownloadSource.kt
+++ plugin/src/main/kotlin/org/danbrough/xtras/git/taskDownloadSource.kt
@@ -26,9 +26,56 @@ abstract class SourceDownload : DefaultTask() {
 }
 */
 
-
 internal fun XtrasLibrary.registerGitSourceDownloadTask(): String {
   val taskName = taskNameSourceDownload()
+  val repoDir = cacheDir
+  val gitConfig = sourceConfig as XtrasLibrary.GitSourceConfig
+  val gitUrl = gitConfig.url
+  val gitCommit = gitConfig.commit
+
+  val writeScriptTaskName = "${taskName}_writeScript"
+  val scriptFile = repoDir.resolve("download.sh")
+  project.tasks.register(writeScriptTaskName) {
+    inputs.property("url", gitUrl)
+    inputs.property("commit", gitCommit)
+    outputs.file(scriptFile)
+    doFirst {
+      if (!repoDir.exists())
+        repoDir.mkdirs()
+    }
+    actions.add {
+      scriptFile.printWriter().use { output ->
+        output.println(
+          """#!/usr/bin/env bash
+cd ${repoDir.absolutePath}
+if [ ! -f HEAD ]; then
+  git init --bare .
+  git remote add origin $gitUrl
+  touch tags.txt
+fi
+if ! grep $gitCommit tags.txt ; then
+  echo fetching $gitCommit from $gitUrl .. 
+  git fetch origin --depth 1 $gitCommit
+  echo `cat FETCH_HEAD  | awk '{print $1}'`  $gitCommit >> tags.txt
+fi
+git reset --soft `grep version-3.51.0 tags.txt  | awk '{print $1}'`
+            """.trim()
+        )
+      }
+    }
+  }
+
+  project.tasks.register<Exec>(taskName) {
+    dependsOn(writeScriptTaskName)
+    group = TaskNames.XTRAS_TASK_GROUP
+    workingDir(repoDir)
+    commandLine("bash", "download.sh")
+  }
+  return taskName
+}
+
+internal fun XtrasLibrary.registerGitSourceDownloadTaskOld(): String {
+  val taskName = "${taskNameSourceDownload()}Old"
   val repoDir: File = cacheDir
   val gitConfig = sourceConfig as XtrasLibrary.GitSourceConfig
   val gitUrl = gitConfig.url
@@ -69,6 +116,7 @@ internal fun XtrasLibrary.registerGitSourceDownloadTask(): String {
   }
 
   //val cmdLine = listOf("git", "fetch", "origin", "--depth", "1", gitConfig.commit.get())
+  //val shallowFile = repoDir.resolve("shallow")
   val gitFetchTaskName = "${taskName}_fetch"
   project.tasks.register<Exec>(gitFetchTaskName) {
     doFirst {
