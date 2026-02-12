9ae6aaa8ef1f19b68288d3f90a03f116b11b721c
Dan Brough <dan@danbrough.org>
2026-02-12 14:03:14 -0600

    update

--- plugin/src/main/kotlin/org/danbrough/xtras/XtrasEnvironment.kt
+++ plugin/src/main/kotlin/org/danbrough/xtras/XtrasEnvironment.kt
@@ -127,6 +127,66 @@ fun XtrasEnvironment.konanEnvironment(
   target ?: return env
 
   val clangArgs = when {
+    HostManager.hostIsMac -> {
+      when (target) {
+        KonanTarget.LINUX_X64 -> "--target=${target.hostTriplet} --gcc-toolchain=${
+          depsDir.resolve(
+            "x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2"
+          )
+        }" + " --sysroot=${
+          depsDir.resolveAll(
+            "x86_64-unknown-linux-gnu-gcc-8.3.0-glibc-2.19-kernel-4.9-2",
+            "x86_64-unknown-linux-gnu",
+            "sysroot"
+          )
+        }"
+
+        KonanTarget.LINUX_ARM64 -> "--target=${target.hostTriplet} --gcc-toolchain=${
+          depsDir.resolve(
+            "aarch64-unknown-linux-gnu-gcc-8.3.0-glibc-2.25-kernel-4.9-2"
+          )
+        }" + " --sysroot=${
+          depsDir.resolveAll(
+            "aarch64-unknown-linux-gnu-gcc-8.3.0-glibc-2.25-kernel-4.9-2",
+            "aarch64-unknown-linux-gnu",
+            "sysroot"
+          )
+        }"
+
+        KonanTarget.MINGW_X64 -> "--target=${target.hostTriplet} --gcc-toolchain=${
+          depsDir.resolve("msys2-mingw-w64-x86_64-2")
+        }" + " --sysroot=${
+          depsDir.resolveAll(
+            "msys2-mingw-w64-x86_64-2", "x86_64-w64-mingw32", "sysroot"
+          )
+        }"
+
+        KonanTarget.ANDROID_ARM64 -> "--target=${target.hostTriplet} --gcc-toolchain=${
+          depsDir.resolve(
+            "target-toolchain-2-linux-android_ndk"
+          )
+        }" + " --sysroot=${
+          depsDir.resolveAll(
+            "target-toolchain-2-linux-android_ndk",
+            "aarch64-linux-android",
+          )
+        }"
+
+        /*        KonanTarget.ANDROID_ARM64 -> "--target=${target.hostTriplet} --gcc-toolchain=${
+                  depsDir.resolve(
+                    "target-toolchain-2-linux-android_ndk"
+                  )
+                }" + " --sysroot=${
+                  depsDir.resolveAll(
+                    "target-toolchain-2-linux-android_ndk",
+                    "aarch64-linux-android",
+                  )
+                }"*/
+
+        else -> error("Unhandled target: $target")
+      }
+    }
+
     HostManager.hostIsLinux -> {
       when (target) {
         KonanTarget.LINUX_X64 -> "--target=${target.hostTriplet} --gcc-toolchain=${
@@ -187,7 +247,11 @@ fun XtrasEnvironment.konanEnvironment(
       }
     }
 
+    HostManager.hostIsMac ->
+
     else -> TODO("Support: ${HostManager.host}")
+
+
   }
 
   env["CLANG_ARGS"] = clangArgs
