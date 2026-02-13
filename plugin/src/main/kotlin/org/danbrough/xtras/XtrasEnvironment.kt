ad50933841180cb7a654987008b5ee79485fd619
Dan Brough <dan@danbrough.org>
2026-02-13 14:37:26 -0600

    update

--- plugin/src/main/kotlin/org/danbrough/xtras/XtrasEnvironment.kt
+++ plugin/src/main/kotlin/org/danbrough/xtras/XtrasEnvironment.kt
@@ -93,8 +93,8 @@ fun XtrasEnvironment.environmentApple(
   target: KonanTarget
 ): ScriptEnvironment {
 
-  env["CFLAGS"] =
-    "-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+  /*  env["CFLAGS"] =
+      "-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"*/
 
   val clangArgs =
     "--target=${target.hostTriplet}"
