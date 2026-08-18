808cda3269e925471371b31b00c5a80e70194350
Dan Brough <dan@danbrough.org>
2026-08-17 20:31:31 -0600

    update

--- libs/ssh2/plugin/src/main/kotlin/org/danbrough/ssh2/plugin/ssh2.kt
+++ libs/ssh2/plugin/src/main/kotlin/org/danbrough/ssh2/plugin/ssh2.kt
@@ -101,7 +101,7 @@ private fun Project.registerSsh2Library() {
         println("if [ ! -f configure ]; then (autoreconf -fiv || exit 1); fi")
         println("if [ ! -f Makefile ]; then")
         println("./configure --prefix=\"${outputDirectory.get()}\" --host=${konanTarget.hostTriplet} \\")
-        println("--with-libssl-prefix=${xtrasLibDir.resolveAll("openssl_${konanTarget.xtrasName}_3.6.3/lib").absolutePath} --with-crypto=openssl  \\")
+        println("--with-crypto=openssl --with-libssl-prefix=${xtrasLibDir.resolveAll("openssl_${konanTarget.xtrasName}_3.6.3/").absolutePath}   \\")
         println("--enable-static=no")
         println("fi || exit 1")
         println("echo source configured .. building in 2")
